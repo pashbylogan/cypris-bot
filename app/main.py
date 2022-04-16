00bae190fc1b3a5becc067831b0c89c285b6dd8b
pashbylogan <pashbylogan@gmail.com>
2022-04-16 04:04:58 +0000

    trying to deploy app

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 import os, json, threading
 from flask import Flask, request, make_response
 from slack import WebClient
-from app.bot import Bot
+from app import Bot
 
 # Initialize a Flask app to host the events adapter
 app = Flask(__name__)
