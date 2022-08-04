ed1ef765b0cfbb40b83c1f5d5d68a0b25a114094
pashbylogan <pashbylogan@gmail.com>
2022-08-04 23:46:43 +0000

    trying to refine query strings

--- bot.py
+++ bot.py
@@ -207,25 +207,35 @@ class Bot:
 
         parsed_q = q.replace(' AND ', ' ')
         parsed_q = parsed_q.replace(' OR ', ' ')
+        parsed_q = parsed_q.replace(')', '')
+        parsed_q = parsed_q.replace('(', '')
+        parsed_q = re.sub('\s+',' ',parsed_q)
 
+        individual_words = []
+        individual_words.extend(re.findall(r'"(.*?)"', parsed_q, re.DOTALL))
+        for word in individual_words : parsed_q.replace(word, '')
         individual_words = parsed_q.split(' ')
         for word in individual_words:
             q = q.replace(word, os.environ['CORE_TEMPLATE_EXACT'].replace('KEYWORD', word.replace('"', '')))
         
         q = f'({q})'
+        print(q)
         return q
 
     def _format_semantic_query (self, query):
         """The semantic query structure as seen in the API docs.
         NOTE - Boolean search is not an option at the time of writing.
         """
+        query = query.replace('"', '')
         query = query.lower()
         query = query.replace(')', '')
         query = query.replace('(', '')
         query = query.replace(' and ', ' ')
         query = query.replace(' or ', ' ')
+        query = re.sub('\s+',' ',query)
         words = query.split(' ')
         words = ' '.join(words)
+        print(words)
         return words
     
     def _replace_ands_ors(self, q):
@@ -285,6 +295,7 @@ class Bot:
             'per_page': per_page,
         }
         data['published_at_start'] = ''.join(['NOW-', str(days_behind), 'DAYS'])
+        print(query)
         data['text'] = query
         if aql:
             data['aql'] = \
@@ -346,6 +357,7 @@ class Bot:
         """
 
         def util(q):
+            q = q.replace('"', '')
             q = q.split(' AND ')
             for i, item in enumerate(q):
                 item = item.replace('(', '')
@@ -353,6 +365,7 @@ class Bot:
                 item = item.replace('-', ' ')
                 q[i] = item
             return q
+        print(util(q))
 
         params = {
             "similarMatch":False,
