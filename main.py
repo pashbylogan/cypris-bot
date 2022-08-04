af46833f8d4f8d7de30ab132db10bc1a71b1e097
pashbylogan <pashbylogan@gmail.com>
2022-08-04 19:05:48 +0000

    trying to make secondaries optional

--- main.py
+++ main.py
@@ -45,6 +45,7 @@ def _send_modal(trigger_id):
 	    		"type": "input",
 	    		"element": {
 	    			"type": "plain_text_input",
+                    "optional": True,
 	    			"action_id": "secondary",
 	    			"placeholder": {
 	    				"type": "plain_text",
