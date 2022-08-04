142c288e445730403559105c881b03ad70e1ff01
pashbylogan <pashbylogan@gmail.com>
2022-08-04 23:27:56 +0000

    allowing quotes in query

--- bot.py
+++ bot.py
@@ -210,7 +210,7 @@ class Bot:
 
         individual_words = parsed_q.split(' ')
         for word in individual_words:
-            q = q.replace(word, os.environ['CORE_TEMPLATE_EXACT'].replace('KEYWORD', word))
+            q = q.replace(word, os.environ['CORE_TEMPLATE_EXACT'].replace('KEYWORD', word.replace('"', '')))
         
         q = f'({q})'
         return q
